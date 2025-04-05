0adb7fe9d57d75b345cc268ac2b5fa67cfa8f621
karungop <karun.gopal@gmail.com>
2025-04-05 15:53:47 -0400

    idk 2345678765

--- frontend/src/teacherAssesment.js
+++ frontend/src/teacherAssesment.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './TeacherAssesment.css';
+import './teacherAssesment.css';
 
 const ConfigureAssessment = () => {
   const [numQuestions, setNumQuestions] = useState(5);
